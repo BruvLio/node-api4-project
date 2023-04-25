6b1a182da611e7bc1072e4de31c73720cdf1e635
BruvLio <lionel.mbayu@gmail.com>
2023-04-25 15:34:00 -0500

    Created endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./server");
+require("dotenv").config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`We are live on ${port}`);
--- server.js
+++ server.js
@@ -4,4 +4,26 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/api/users", (req, res) => {
+  res.json("hello, there");
+});
+
+server.post("/api/register", (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    res.status(201).json(req.body);
+  } else {
+    res.status(401).json({ message: "Must have username and password" });
+  }
+});
+
+server.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    res.status(201).json(req.body);
+  } else {
+    res.status(401).json({ message: "Welcome" });
+  }
+});
+
 module.exports = server;
